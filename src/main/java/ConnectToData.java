d2fbd5c5d4d224a346c2f317077c72c67ea57099
raphss <raphvilete@gmail.com>
2024-04-13 06:45:34 -0300

    version 1.0

---
+++ src/main/java/ConnectToData.java
@@ -0,0 +1,42 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * A utility class for establishing a database connection.
+ * 
+ * The database connection parameters (database name, URL, username, password)
+ * are defined here.
+ * 
+ * @author Raphael Vilete
+ */
+
+public class ConnectToData {
+
+    /**
+     * Get a database connection.
+     * 
+     * @return A database connection object.
+     * @throws SQLException If a database-related error occurs.
+     */
+
+    public Connection getConnection() throws SQLException {
+
+        Connection connection = null;
+
+        try {
+
+            String dbName = "test";
+            String dbUrl = "jdbc:h2:./src/main/resources/" + dbName;
+
+            // Establish a connection to the database with the specified URL, username, and
+            // password
+            connection = DriverManager.getConnection(dbUrl, "root", "root");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+}
